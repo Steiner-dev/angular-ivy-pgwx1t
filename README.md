e1d6d51a3d8a30adc962a0c608cc2b04e0652979
Steiner-dev <67967945+Steiner-dev@users.noreply.github.com>
2022-12-19 15:47:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-pgwx1t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-pgwx1t)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import { MatProgressBarModule } from '@angular/materiel';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+    MatProgressBarModule
+  ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
